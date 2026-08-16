49233eb71b8fff6fb7e34bc2a8106e51038dcd28
miguel-aqs <mrmirosilva@gmail.comm>
2026-08-16 17:19:46 +0800

    Implemented Wolff algorithm.

--- ising.py
+++ ising.py
@@ -6,18 +6,18 @@ from matplotlib.widgets import Slider, TextBox
 from matplotlib.colors import ListedColormap
 
 """---CONFIG STUFF---"""
-grid_size = 75
+grid_size = 250
 initial_state = 0 # 0 = completely random, 1 = all +1 spin, -1 = all -1 (no in between)
 pctg_impurities = 0.39
 
-T = 1.9 # initial temperature
-steps_per_frame = 100000
+T = 2.269 # initial temperature
+steps_per_frame = 10
 
 show_graph = True
 graph_length = 100
 
 show_bottom_controls = True
-input_mode = 1 # 0 = Use slider, 1 = Use text box
+input_mode = 0 # 0 = Use slider, 1 = Use text box
 
 """------------------"""
 
@@ -37,33 +37,49 @@ def init_lattice(N):
         spins = np.random.choice([0, -1, 1], size=(N, N), p=[pctg_impurities, p_spin, 0])
     return spins
 
-def get_neighbors_sum(spins, i, j, N):
-    top = spins[(i-1) % N, j]
-    bottom = spins[(i+1) % N, j]
-    left = spins[i, (j-1) % N]
-    right = spins[i, (j+1) % N]
-    
-    return top + bottom + left + right
 
 
-def metropolis(spins, N, T, J=1.0):
+def wolff(spins, N, T, J=1.0):
     i = random.randint(0, N-1)
     j = random.randint(0, N-1)
 
     if spins[i, j] == 0:
         return
 
-    current_spin = spins[i, j]
-    neighbors_sum = get_neighbors_sum(spins, i, j, N)
+    seed = spins[i, j]
 
-    dE = 2 * J * current_spin * neighbors_sum
+    prob = np.exp((-2 * J) / T)
+    P_add = 1 - prob
 
-    if dE <= 0 or random.random() < np.exp(-dE / T):
-        spins[i, j] *= -1
+    cluster = set([(i, j)])
 
-def get_net_magnetization(spins):
+    stack = [(i, j)]
+
+    while stack:
+        current_i, current_j = stack.pop()
+
+        neighbors = [
+            ((current_i + 1) % N, current_j),
+            ((current_i - 1) % N, current_j),
+            (current_i, (current_j + 1) % N),
+            (current_i, (current_j - 1) % N)
+        ]
+
+        for ni, nj in neighbors:
+            if spins[ni, nj] == seed:
+                if (ni, nj) not in cluster:
+                    if random.random() < P_add:
+                        cluster.add((ni, nj))
+                        stack.append((ni, nj))
+
+    for oi, oj in cluster:
+        spins[oi, oj] *= -1
+        
+    
+
+def get_abs_magnetization(spins):
     active_spins = spins[spins != 0]
-    return np.mean(active_spins) if len(active_spins) != 0 else 0.0
+    return abs(np.mean(active_spins)) if len(active_spins) != 0 else 0.0
 
 
 
@@ -95,9 +111,9 @@ if show_graph:
     line, = ax2.plot([], [], color='black')
     ax2.set_xlim(0, graph_length)
     ax2.get_xaxis().set_visible(False)
-    ax2.set_ylim(-1.1, 1.1)
+    ax2.set_ylim(-0.1, 1.1)
 
-    graph_title = ax2.set_title("Net Magnetization", fontsize=10)
+    graph_title = ax2.set_title("Absolute Magnetization", fontsize=10)
 
 if show_bottom_controls:
     ax_control = fig.add_subplot(gs[1, :]) 
@@ -123,16 +139,16 @@ def update(frame):
         current_T = T
     
     for _ in range(steps_per_frame):
-        metropolis(grid1, grid_size, current_T)
+        wolff(grid1, grid_size, current_T)
     
     im.set_data(grid1)
 
     if show_graph:
-        net_mag = get_net_magnetization(grid1)
+        net_mag = get_abs_magnetization(grid1)
         mag_history.append(net_mag)
     
         line.set_data(range(len(mag_history)), mag_history)
-        if len(mag_history) > 100:
+        if len(mag_history) > graph_length:
             ax2.set_xlim(0, len(mag_history))
         
         graph_title.set_text("Net Magnetization (M):")
@@ -143,6 +159,6 @@ def update(frame):
     
     
 
-ani = animation.FuncAnimation(fig, update, interval=10, blit=True, cache_frame_data=False)
+ani = animation.FuncAnimation(fig, update, interval=5, blit=True, cache_frame_data=False)
 
 plt.show()
